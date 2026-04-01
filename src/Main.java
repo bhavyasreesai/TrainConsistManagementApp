243dd2f9bd7527e8ef8e9c09489e5dbca285ed58
bhavyasreesai <chinasanibhavya@gmail.com>
2026-04-01 08:28:27 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+package TrainConsistManagement;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the TrainConsistManagement System");
+        System.out.println("TrainConsistManagement initialized Successfully");
+    }
+}
