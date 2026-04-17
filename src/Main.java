61fb305730a772d3db0afad2bb393e06394982c5
bhavyasreesai <chinasanibhavya@gmail.com>
2026-04-17 10:06:13 +0530

    Sort Bogie Names Using Arrays.sort()

--- src/Main.java
+++ src/Main.java
@@ -1,40 +1,24 @@
 package TrainConsistManagement;
-
+import java.util.Arrays;
  class TrainConsistManagementApp {
     public static void main(String[] args) {
-        int[] bogieCapacities = {72, 56, 24, 70, 60};
+        String[] bogieNames = {"Luxury", "General", "Sleeper", "AC Chair", "First Class"};
 
         System.out.println("Before Sorting:");
-        displayCapacities(bogieCapacities);
+        displayBogieNames(bogieNames);
 
-        bubbleSort(bogieCapacities);
+        // Sort bogie names alphabetically using Arrays.sort()
+        Arrays.sort(bogieNames);
 
         System.out.println("\nAfter Sorting:");
-        displayCapacities(bogieCapacities);
+        displayBogieNames(bogieNames);
     }
 
-    // Bubble Sort algorithm
-    public static void bubbleSort(int[] capacities) {
-        int n = capacities.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = 0; j < n - i - 1; j++) {
-                if (capacities[j] > capacities[j + 1]) {
-                    // Swap
-                    int temp = capacities[j];
-                    capacities[j] = capacities[j + 1];
-                    capacities[j + 1] = temp;
-                }
-            }
-        }
-    }
-
-    // Display bogie capacities
-    public static void displayCapacities(int[] capacities) {
-        for (int capacity : capacities) {
-            System.out.print(capacity + " ");
-        }
-        System.out.println();
-    }
-}
-
-
+     // Display bogie names
+     public static void displayBogieNames(String[] bogies) {
+         for (String bogie : bogies) {
+             System.out.print(bogie + " , ");
+         }
+         System.out.println();
+     }
+ }
